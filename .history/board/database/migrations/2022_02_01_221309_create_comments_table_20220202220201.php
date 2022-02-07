60297bdd803dcfeb5cfba3f538af1c400f9f133c
rrraaa890 <ryu890b@yahoo.co.jp>
2022-02-07 23:45:38 +0900

    “first”

---
+++ .history/board/database/migrations/2022_02_01_221309_create_comments_table_20220202220201.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('post_id');
+
+            $table->string('body');
+
+            $table->foreign('user_id')->references('id')->on('user');
+            $table->foreign()->references('id')->on('post');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
